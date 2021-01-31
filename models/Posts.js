8c7bf58d8b8cdb3a47de82cd986799ae8f526c0e
Almaz <atunstyle@gmail.com>
2021-01-31 19:20:36 +0300

    rewrite and rename model *Rewrite Quotes to Post model

--- models/Posts.js
+++ models/Posts.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const QuotesSchema = new Schema({
+const PostsSchema = new Schema({
     body: String,
     userName: String,
     createdAt: String,
     user: Schema.Types.ObjectId
 })
 
-module.exports = mongoose.model('Quote', QuotesSchema)
+module.exports = mongoose.model('Post', PostsSchema)
