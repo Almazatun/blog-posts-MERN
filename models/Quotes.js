30842cbb92b85b274d33db3ce6fb9ee8aca48c62
Almaz <atunstyle@gmail.com>
2021-01-31 18:01:39 +0300

    modify model type *Included other key properties of the Quotes model

--- models/Quotes.js
+++ models/Quotes.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const QuotesSchema = new Schema({
-    content: String,
-    author: String
+    body: String,
+    userName: String,
+    createdAt: String,
+    user: Schema.Types.ObjectId
 })
 
 module.exports = mongoose.model('Quote', QuotesSchema)
