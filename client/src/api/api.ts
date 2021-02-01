0ca9005b6c5cd1a572fee9340bae0beafb728dd5
Almaz <atunstyle@gmail.com>
2021-02-01 08:09:13 +0300

    rewrite key word and type *Rewrite all Quotes | Quote key word to Posts | Post *Updated types

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,33 +1,31 @@
-import axios from "axios";
-import {BASE_URL} from "../confirm/apiData";
+import {instance} from "../confirm/apiData";
 
-//Axios
-export const instance = axios.create({
-    baseURL: BASE_URL,
-})
 
 //API
 export const API = {
-    getAllQuotes(){
-        return instance.get<Array<QuoteServe>>('').then(res => {
-          return res.data
+    getAllPosts() {
+        return instance.get<Array<PostServe>>('').then(res => {
+            return res.data
         })
     },
-    createQuote(data: QuoteServe){
-        return instance.post<QuoteServe>('new/', data).then(res => res.data)
+    createPost(data: PostServe) {
+        return instance.post<PostServe>('new/', data).then(res => res.data)
     },
-    deleteQuote(quoteId: string ){
-        return instance.delete<QuoteServe>(`delete/${quoteId}`).then(res => res.data)
+    deletePost(postId: string) {
+        return instance.delete<PostServe>(`delete/${postId}`).then(res => res.data)
     },
-    updateQuote(quoteId: string , title: string, ){
-        const data: {content: string} = {content: title}
-        return instance.patch<QuoteServe>(`update/${quoteId}`, data)
+    updatePost(postId: string, title: string,) {
+        const data: { content: string } = {content: title}
+        return instance.patch<PostServe>(`update/${postId}`, data)
     }
 }
 
 //Types
-export interface QuoteServe {
+export interface PostServe {
     _id: string
-    content: string,
-    author: string
+    body: string,
+    userName: string,
+    createdAt: string,
+    user: string,
+    __v?: number
 }
