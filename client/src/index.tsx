af95405d0c87d4c256f8be507979c4dea5b28824
Almaz <atunstyle@gmail.com>
2021-02-03 13:32:13 +0300

    wrap root component with data sharing providers *Wrapped App component with Redux Provider as well as AuthProvider.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './appClient/App';
 import './index.scss';
 import {Provider} from 'react-redux'
 import reportWebVitals from './reportWebVitals';
-import { store } from './reducer';
+import {store} from './reducer';
+import {AuthProvider} from "./context/AuthProvider";
+import {BrowserRouter} from "react-router-dom";
+import App from "./appClient/App";
 
 ReactDOM.render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <App/>
-      </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <AuthProvider>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </AuthProvider>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
